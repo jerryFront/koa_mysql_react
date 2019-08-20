82c2c2ed00c9f9ceef5782198b925ae930078276
骑鲨鱼去远航 <1162257086@qq.com>
2019-08-20 23:20:39 +0800

    补提交接口

--- server/router/music.js
+++ server/router/music.js
@@ -13,7 +13,16 @@ const apis=[
     'playlist/detail', //推荐歌单的详情
     'top/artists', //歌手列表(默认分页)
     'artist/list', //字母选择歌手
+    'artist/album', //获取歌手专辑(id，limit)
     'toplist/detail', //排行榜相关
+    'top/playlist', //歌单 ( 网友精选碟 )
+    'top/playlist/highquality', //获取精品歌单
+    'lyric', //获取歌词(id)
+    'related/playlist', //相关歌单的推荐歌曲(id)
+    'album', //获取专辑内容(id)
+    'album/detail/dynamic', //专辑动态信息(id)
+    'simi/playlist', //获取相似歌单(id)
+    'simi/song', //获取相似音乐(id)
 ]
 
 apis.forEach(url=>{
